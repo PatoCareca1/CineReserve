b2cc7bc0341cb8bb37ac67d3826f9ab06bae1ad1
Lucas Daniel <LucasDan16@outlook.com>
2026-03-17 10:17:04 -0300

    security: implement rate limiting for api endpoints

--- src/core/settings.py
+++ src/core/settings.py
@@ -136,6 +136,14 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '20/min',
+        'user': '100/min'
+    }
 }
 
 SPECTACULAR_SETTINGS = {
